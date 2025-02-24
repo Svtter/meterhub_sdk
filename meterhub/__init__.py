a4a4634e4113d8379e196c8e34711906a1c5b047
svtter <svtter@qq.com>
2025-02-24 14:29:22 +0800

    add dynamic version.

--- meterhub/__init__.py
+++ meterhub/__init__.py
@@ -1,7 +1,28 @@
+"""
+MeterHub is a Python library for detecting and handling meter images,
+using API of [MeterHub](https://meterhub.sun-praise.com).
+
+__author__ = "Sun Praise"
+__email__ = "support@sun-praise.com"
+__url__ = "https://meterhub.sun-praise.com"
+__date__ = "2025-02-21"
+"""
+
+__version__ = "0.1.0"
+
+from PIL import Image
+import pathlib
 
 class MeterHub:
   def __init__(self, api_key: str):
     self.api_key = api_key
 
-  def get_meter_data(self, meter_id: str):
-    pass
+  def handle_file(self, file: str | pathlib.Path | Image.Image):
+    if isinstance(file, str | pathlib.Path):
+      return Image.open(file)
+    return file
+
+  def detection(self, image: str | pathlib.Path | Image.Image):
+    image = self.handle_file(image)
+
+
